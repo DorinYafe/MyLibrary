0e322196020f7faa453b28fc3413f7f587092940
dorin yafe <you@example.com>
2021-08-09 16:15:39 +0300

    initial commit again

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect database
+connectDB();
+
+//Init middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('API Running'));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/books', require('./routes/api/books'));
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
